d9416fcf3082b284913429b14afd1a9de584418e
quocbao19982009 <86527891+quocbao19982009@users.noreply.github.com>
2021-11-14 13:00:08 +0200

    styling navbar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import PlayIcon from "../images/play-button.svg";
 import requests from "../api/requests";
 import classes from "./Banner.module.css";
 
@@ -21,15 +23,26 @@ const Banner = () => {
     <header
       className={classes.banner}
       style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"`,
+        backgroundImage: `linear-gradient(
+          180deg,
+          transparent,
+          rgba(37, 37, 37, 0.61),
+          #111
+        ),url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"`,
       }}
     >
       <div className={classes.banner__contents}>
         <h1 className={classes.banner__title}>{movie.name}</h1>
         <h2 className={classes.banner__description}>{movie.overview}</h2>
         <div className={classes.banner__buttons}>
-          <button className={classes.banner__button}>Play</button>
-          <button className={classes.banner__button}>My List</button>
+          <button className={classes.banner__button}>
+            <i className="fas fa-play"></i>
+            Play
+          </button>
+          <button className={classes.banner__button}>
+            <i className="fas fa-plus"></i>
+            My List
+          </button>
         </div>
       </div>
       {/* <div className={classes["banner--fadebottom"]}></div> */}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,30 +5,56 @@ import netflixLogo from "../images/logo.png";
 const Nav = () => {
   const [show, handleShow] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    });
-    return () => {
-      window.removeEventListener("scroll");
-    };
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     if (window.scrollY > 100) {
+  //       handleShow(true);
+  //     } else handleShow(false);
+  //   });
+  //   return () => {
+  //     window.removeEventListener("scroll");
+  //   };
+  // }, []);
 
   return (
     <nav className={`${classes.nav} ${show && classes.navBackground}`}>
       <div className={classes.container}>
-        <img
-          className={classes.nav__logo}
-          src={netflixLogo}
-          alt="Netflix Logo"
-        ></img>
-        <img
-          className={classes.nav__avatar}
-          alt="Your Avatar Netflix"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-        ></img>
+        <div className={classes.nav__rightside}>
+          <img
+            className={classes.nav__logo}
+            src={netflixLogo}
+            alt="Netflix Logo"
+          ></img>
+          <ul className={classes.nav__links}>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>TV Shows</a>
+            </li>
+            <li>
+              <a>Movies</a>
+            </li>
+            <li>
+              <a>Recently Added</a>
+            </li>
+            <li>
+              <a>My List</a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.nav__leftside}>
+          <div className={classes.search}>
+            <i className={`fas fa-search ${classes.iconSearch}`} />
+            <input className={`${classes.searchBox} ${classes.hide}`}></input>
+          </div>
+          <i className={`fas fa-bell ${classes.iconBell}`} />
+          <img
+            className={classes.nav__avatar}
+            alt="Your Avatar Netflix"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          ></img>
+        </div>
       </div>
     </nav>
   );
